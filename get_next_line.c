c1d35d022ae53abee414b1b1f7e7bab080f679ac
Nicolas TEROL <nterol@e1z3r1p18.42.us.org>
2017-02-15 15:54:36 -0800

    It works, but it's basic

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,52 @@
 /*   By: nterol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 10:00:23 by nterol            #+#    #+#             */
-/*   Updated: 2017/02/08 12:04:12 by nterol           ###   ########.fr       */
+/*   Updated: 2017/02/13 20:38:21 by nterol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+int		my_func(char **line, char **tmp)
 {
-	int		i;
-	int		ret;
-	char	buf[BUFF_SIZE];
-
-	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
-		return (-1);
-	*line = ft_strnew(1);
-	while (ret = read(fd, buf, BUFF_SIZE))
-	{
-		buf[ret] = '\0';
-		if (ft_strchr(buf, CHAR))
-			break;
-	}
+		char *o;
+		if ((o = ft_strchr(*tmp, CHAR)))
+		{
+			*line = ft_strsub(*tmp, 0, o - *tmp);
+			*tmp = ft_strdup(o + 1);
+			return (1);
+		}
+		return (0);
 }
 
+int		get_next_line(const int fd, char **line)
+{
+	int				ret;
+	static char		*tmp = NULL;
+	char			buf[BUFF_SIZE + 1];
+
+	ret = 0;
+	if (tmp == NULL)
+		tmp = ft_strnew(0);
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+		return (-1);
+	*line = ft_strnew(0);
+	if (my_func(line, &tmp) == 1)
+		return (1);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		tmp = ft_strjoin(tmp, buf);
+		if(my_func(line, &tmp) == 1)
+			return (1);
+			if (ret < BUFF_SIZE && !(ft_strlen(tmp)))
+				return (0);
+	}
+	if (ft_strlen(tmp))
+	{
+		*line = ft_strdup(tmp);
+		free(tmp);
+		return (1);
+	}
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nterol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 09:39:30 by nterol            #+#    #+#             */
-/*   Updated: 2017/02/08 10:20:42 by nterol           ###   ########.fr       */
+/*   Updated: 2017/02/13 20:37:26 by nterol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # define CHAR '\n'
 
 # include "libft/libft.h"
+# include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
+int		main(int ac, char **av);
 
 #endif
