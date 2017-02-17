eab226ffffc40de081568c051622bcd8257dafc1
Nicolas TEROL <nterol@e1z3r1p18.42.us.org>
2017-02-16 19:05:13 -0800

    Passes all the tests except multi fd which I won\'nt do

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,48 @@
 /*   By: nterol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 10:00:23 by nterol            #+#    #+#             */
-/*   Updated: 2017/02/13 20:38:21 by nterol           ###   ########.fr       */
+/*   Updated: 2017/02/16 18:51:43 by nterol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		my_func(char **line, char **tmp)
+int				check_n_fill(char **line, char **tmp)
 {
-		char *o;
-		if ((o = ft_strchr(*tmp, CHAR)))
-		{
-			*line = ft_strsub(*tmp, 0, o - *tmp);
-			*tmp = ft_strdup(o + 1);
-			return (1);
-		}
-		return (0);
+	char		*o;
+
+	if ((o = ft_strchr(*tmp, CHAR)))
+	{
+		*line = ft_strsub(*tmp, 0, o - *tmp);
+		*tmp = ft_strdup(o + 1);
+		return (1);
+	}
+	return (0);
 }
 
-int		get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	int				ret;
-	static char		*tmp = NULL;
-	char			buf[BUFF_SIZE + 1];
+	int			ret;
+	static char	*tmp = NULL;
+	char		buf[BUFF_SIZE + 1];
 
-	ret = 0;
-	if (tmp == NULL)
-		tmp = ft_strnew(0);
+	!tmp ? tmp = ft_strnew(0) : 0;
 	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
 		return (-1);
 	*line = ft_strnew(0);
-	if (my_func(line, &tmp) == 1)
+	if (check_n_fill(line, &tmp) == 1)
 		return (1);
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
 		tmp = ft_strjoin(tmp, buf);
-		if(my_func(line, &tmp) == 1)
+		if (check_n_fill(line, &tmp) == 1)
 			return (1);
-			if (ret < BUFF_SIZE && !(ft_strlen(tmp)))
-				return (0);
 	}
-	if (ft_strlen(tmp))
+	if (ft_strcmp(tmp, ""))
 	{
 		*line = ft_strdup(tmp);
+		tmp = NULL;
 		free(tmp);
 		return (1);
 	}
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,18 @@
 /*   By: nterol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 09:39:30 by nterol            #+#    #+#             */
-/*   Updated: 2017/02/13 20:37:26 by nterol           ###   ########.fr       */
+/*   Updated: 2017/02/15 20:12:43 by nterol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 17
 # define CHAR '\n'
 
 # include "libft/libft.h"
 # include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
-int		main(int ac, char **av);
 
 #endif
